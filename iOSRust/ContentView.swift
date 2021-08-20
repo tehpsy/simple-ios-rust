b6452f218ebfd063438e59daa7e4d179fc918a1c
James Baxter <james.baxter@focusrite.com>
2021-08-20 15:09:46 +0800

    Initial Commit

---
+++ iOSRust/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOSRust
+//
+//  Created by James Baxter on 20/08/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOSRust/iOSRustApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSRustApp.swift
+//  iOSRust
+//
+//  Created by James Baxter on 20/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct iOSRustApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
